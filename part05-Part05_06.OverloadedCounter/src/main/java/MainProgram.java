13afdb72e5c2a365fd22570f86c6a79726386fa1
anrixs <anrijs.avins@gmail.com>
2025-08-20 18:59:07 +0300

    Part 5, Exercise 6 – Overloaded Counter (finished)

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,10 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter c = new Counter(2);
+        c.decrease();
+        c.decrease();
+        c.decrease();
+        System.out.println(c.value());
     }
 }
