da29c49e8cf3f63aee405d69426b81758c9b6afb
anrixs <anrijs.avins@gmail.com>
2025-08-21 17:42:17 +0300

    Part 5, Exercise 7 – NullPointerException (finished)

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -4,9 +4,10 @@ public class MainProgram {
     public static void main(String[] args) {
         // Test your counter here
         Counter c = new Counter(2);
+        c.decrease(); //calling the decrease() method
         c.decrease();
         c.decrease();
-        c.decrease();
+        c.decrease(3); //calling the decrease(int decreaseBy) method
         System.out.println(c.value());
     }
 }
