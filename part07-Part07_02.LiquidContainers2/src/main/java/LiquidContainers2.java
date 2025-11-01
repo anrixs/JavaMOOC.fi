8d505a7a9fa95bb1e20ee1c817ecb6ec9228b7c9
anrixs <anrijsavins@gmail.com>
2025-11-01 17:55:14 +0200

    Part 7, Exercise 2 - Liquid Containers 2.0 (finished)

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -6,15 +6,33 @@ public class LiquidContainers2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        Container first = new Container();
+        Container second = new Container();
 
         while (true) {
-            System.out.print("> ");
-
+            System.out.println("First: " + first.toString() + "\n" + "Second: " + second.toString());
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
 
+            if (command.equals("add")) {
+                first.add(amount);
+            }
+
+            if (command.equals("remove")) {
+                second.remove(amount);
+            }
+
+            if (command.equals("move")) {
+                int amountToMove = Math.min(amount, first.contains());
+                
+                first.remove(amountToMove);
+                second.add(amountToMove);
+            }
         }
     }
 
