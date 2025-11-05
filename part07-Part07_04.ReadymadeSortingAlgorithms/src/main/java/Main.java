96036878e64492d05f3d199cb7791021c58e2713
anrixs <anrijsavins@gmail.com>
2025-11-05 15:31:14 +0200

    Part 7, Exercise 4 - Ready made Sorting Algorithms (finished)

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,48 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        //int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers1 = {8, 3, 7, 9, 1, 2, 4};
+        System.out.println(Arrays.toString(numbers1));
+        Main.sort(numbers1);
+        System.out.println(Arrays.toString(numbers1));
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(8);
+        numbers.add(3);
+        numbers.add(7);
+        System.out.println(numbers);
+        Main.sortIntegers(numbers);
+        System.out.println(numbers);
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("What did you eat at breakfast?");
+        strings.add("How was your day?");
+        strings.add("Hello World!");
+        System.out.println(strings);
+        Main.sortStrings(strings);
+        System.out.println(strings);
     }
 
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
+    }
 }
